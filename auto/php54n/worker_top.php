46211e440db36a3c5d0194f52decad4865fb9f43
liushuna <1571644279@qq.com>
2019-04-18 17:22:06 +0800

    提交

---
+++ auto/php54n/worker_top.php
@@ -0,0 +1,10 @@
+<?php
+$taskid = $argv[3];
+$dk = $argv[4];
+$db = array();
+echo $dk;
+$server = array();
+ouput("读取配置");
+$typelist = array();
+$yslist = array();
+$worker = new Worker('websocket://0.0.0.0:' . $dk);
